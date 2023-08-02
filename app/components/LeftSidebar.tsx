dbe0fb4dfea37f0db7a477ec495064ea0e06cb7c
Tony <greybluesea@gmail.com>
2023-08-03 11:06:42 +1200

    minor styling change

--- app/components/LeftSidebar.tsx
+++ app/components/LeftSidebar.tsx
@@ -6,6 +6,7 @@ import { FaEnvelope, FaMagnifyingGlass, FaUserLarge } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
 
 import { GoBellFill } from "react-icons/go";
+import UserSection from "./UserSection";
 
 const navItemsArray = [
   {
@@ -36,11 +37,11 @@ const navItemsArray = [
 
 const LeftSidebar = () => {
   return (
-    <aside
-      className="w-70 p-4 text-slate-300 flex flex-col justify-between text-xl border-r-1 border-slate-600"
+    <nav
+      className=" w-70 h-screen p-4 text-slate-300 flex flex-col justify-between text-xl border-r-1 border-slate-600"
       style={{ borderRightWidth: "1px" }}
     >
-      <nav
+      <div
         id="LeftSidebar"
         className="w-full flex flex-col items-start space-y-1 "
       >
@@ -51,36 +52,30 @@ const LeftSidebar = () => {
         >
           <BsTwitter className="w-12 h-12 text-sky-500" />
         </Link>
-        {navItemsArray.map((item) => (
-          <Link
-            key={item.title}
-            id={item.title}
-            href={`/${item.title.toLocaleLowerCase()}`}
-            className="flex items-center hover-bg-light py-3 px-8 rounded-full "
-          >
-            <item.icon className="w-9 h-9" />
-            <p className="pl-4">{item.title}</p>
-          </Link>
-        ))}
-
+        <ul className="flex flex-col">
+          {navItemsArray.map((item) => (
+            <li>
+              <Link
+                key={item.title}
+                id={item.title}
+                href={`/${item.title.toLocaleLowerCase()}`}
+                className="flex items-center hover-bg-light py-3 px-8 rounded-full "
+              >
+                <item.icon className="w-9 h-9" />
+                <p className="pl-4">{item.title}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
         <button
-          className="w-full mx-2  bg-sky-600 p-4 rounded-full text-center hover:bg-opacity-70 transition duration-200"
+          className="w-60 mx-2  bg-sky-600 p-4 rounded-full text-center hover:bg-opacity-70 transition duration-200"
           style={{ marginTop: "32px" }}
         >
           Tweet
         </button>
-      </nav>
-      <button className="w-full mx-2 mt-8 p-4 px-6 rounded-full hover-bg-light  cursor-pointer flex flex-row justify-between items-center">
-        <div className="flex truncate space-x-2">
-          <CgProfile className="w-10 h-10" />
-          <div className="text-left text-sm max-w-20 truncate">
-            <p className="flex font-semibold truncate">CurrentUser</p>
-            <p className="flex truncate max-w-20">@CurrentUser</p>
-          </div>
-        </div>
-        <BsThreeDots className="m-0" />
-      </button>
-    </aside>
+      </div>
+      <UserSection />
+    </nav>
   );
 };
 
