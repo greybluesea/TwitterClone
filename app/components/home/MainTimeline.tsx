9adef916113e83a874bbfd8bd35800f23ee3c550
Tony <greybluesea@gmail.com>
2023-08-04 15:45:27 +1200

    minor

--- app/components/home/MainTimeline.tsx
+++ app/components/home/MainTimeline.tsx
@@ -5,7 +5,13 @@ type Props = {};
 const MainTimeline = (props: Props) => {
   return (
     <div className="flex flex-col w-full h-full min-h-screen">
-      <h1 className="text-xl font-bold p-6  sticky top-0 bg-slate-900">Home</h1>
+      <h1
+        className={
+          "text-xl font-bold p-6 " /* + " sticky top-0 bg-slate-900 " */
+        }
+      >
+        Home
+      </h1>
       <TweetComposer />
     </div>
   );
